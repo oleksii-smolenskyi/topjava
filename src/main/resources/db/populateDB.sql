6bb7ae741afb3fa81886aca64c4992dbc5ef9378
Oleksii <oleksiy.smolenskiy@gmail.com>
2019-11-18 08:00:58 +0200

    fix tests and update sql scripts for table meals

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -9,3 +10,13 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+
+INSERT INTO meals (user_id, datetime, description, calories) VALUES
+  (100000, '17.11.2019T11:04:15.215', 'Сніданок', 650),
+  (100000, '17.11.2019T16:04:15.215', 'Обід', 450),
+  (100000, '17.11.2019T22:04:15.215', 'Вечеря', 700),
+  (100001, '17.11.2019T11:04:15.215', 'Сніданок', 550),
+  (100001, '17.11.2019T16:04:15.215', 'Обід', 750),
+  (100001, '17.11.2019T22:04:15.215', 'Вечеря', 800);
+
